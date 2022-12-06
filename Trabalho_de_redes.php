73d6074d0aaea5732bd14c5d5ceffc70eecec60c
José Barros Bento de Freitas <josezebento5@gmail.com>
2022-12-05 21:43:31 -0300

    Create Trabalho_de_redes.php

---
+++ Trabalho_de_redes.php
@@ -0,0 +1,18 @@
+<h1>Trabalho de redes</h1>
+
+<?php
+	echo nl2br("
+	Alunos: José Barros, Nícolas Tasquetto e Rafael Tischler
+	Turma: 3º MSI b
+	
+	Este projeto foi realizado para uma atividade da diciplina de redes que consistia em criar um servidor do tipo LAMP, Linux, Appax, MySQL e PHP, e hospedar um site criado pelo grupo, além do mais, nele, deve-se por as principais dificuldades que o grupo enfrentou e o seu feedback sobre a atividade
+	
+	Apartir disso, o grupo começa a falar suas difuculdades, que seriam:
+		-
+		-
+		-
+
+	Porém, diante das dificuldades do trabalho e das experiencias obtidas, o grupo tira a seguinte conclusão da atividade:
+		
+		")
+?>
