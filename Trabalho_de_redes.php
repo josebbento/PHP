a883396db202abe640944a53c9b0ff75c37b290c
José Barros Bento de Freitas <josezebento5@gmail.com>
2022-12-05 23:48:51 -0300

    af

--- Trabalho_de_redes.php
+++ Trabalho_de_redes.php
@@ -1,6 +1,6 @@
 <title>Trab. De Redes</title>
 
-<h1>Trabalho de redes</h1>
+<h1>Trabalho de Rede de Computadores</h1>
 
 <?php
 	echo nl2br("
@@ -9,13 +9,16 @@
 	
 	Este projeto foi realizado para uma atividade da diciplina de redes que consistia em criar um servidor do tipo LAMP, Linux, Appax, MySQL e PHP, e hospedar um site criado pelo grupo, além do mais, nele, deve-se por as principais dificuldades que o grupo enfrentou e o seu feedback sobre a atividade
 	
-	Apartir disso, o grupo começa a falar suas difuculdades, que seriam:
+	Com base discusões, o grupo começa a falar suas difuculdades, que seriam:
 		-
 		-
 		-
 
-	Porém, diante das dificuldades do trabalho e das experiencias obtidas, o grupo tira a seguinte conclusão da atividade:
+	A parti das dificuldades do trabalho e das experiencias obtidas, o grupo tira a seguinte conclusão da atividade:
 		-
 		-
-		-")
+		-
+		")
 ?>
+
+<img src="Tux.png">
