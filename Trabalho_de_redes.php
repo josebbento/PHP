a6e197aed08bbb8a2ee5da16f9ebf294fc1bb161
José Barros Bento de Freitas <josezebento5@gmail.com>
2022-12-06 15:31:54 -0300

    fsda

--- Trabalho_de_redes.php
+++ Trabalho_de_redes.php
@@ -1,24 +1,49 @@
 <title>Trab. De Redes</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<style>
+		body {
+			background-image: url("download (1).jpeg");
+		}
+	</style>
+</head>
+<body>
+	
+</body>
+</html>
+
+
+
 
 <h1>Trabalho de Rede de Computadores</h1>
 
 <?php
-	echo nl2br("
-	Alunos: José Barros, Nícolas Tasquetto e Rafael Tischler
-	Turma: 3º MSI b
-	
-	Este projeto foi realizado para uma atividade da diciplina de redes que consistia em criar um servidor do tipo LAMP, Linux, Appax, MySQL e PHP, e hospedar um site criado pelo grupo, além do mais, nele, deve-se por as principais dificuldades que o grupo enfrentou e o seu feedback sobre a atividade
-	
-	Com base discusões, o grupo começa a falar suas difuculdades, que seriam:
-		-
-		-
-		-
+	echo nl2br("Alunos: José Barros, Nícolas Tasquetto e Rafael Tischler
+	Turma: 3º MSI b")
+?>
+<h2>Introdução:</h2>
 
-	A parti das dificuldades do trabalho e das experiencias obtidas, o grupo tira a seguinte conclusão da atividade:
-		-
-		-
-		-
-		")
+<?php
+	echo nl2br("Este projeto foi realizado para uma atividade da diciplina de redes que consistia em criar um servidor do tipo LAMP, Linux, Appax, MySQL e PHP, e hospedar um site criado pelo grupo, além do mais, nele, deve-se por as principais dificuldades que o grupo enfrentou e o seu feedback sobre a atividade")
+?>
+	
+<h2>Dificuldades:</h2>
+
+<?php
+	echo nl2br("- Baixar distro Linux com internet de SVS
+				- Criar uma maquina virtual
+				- Dowload e instalação do servidor LAMP
+				- Achar os comandos certos para usar no terminal linux
+				- Aprender uma linguagem de programação nova
+				- Aprender comandos básicos de HTML")
+?>
+
+<h2>Feedback:</h2>
+
+<?php
+	echo nl2br("- 
+	")
 ?>
 
 <img src="Tux.png">
